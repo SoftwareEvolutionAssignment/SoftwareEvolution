a6bc5aefe63c9984b8a18767449a836e5011c788
masembe <34134777+masembe@users.noreply.github.com>
2018-03-22 23:57:38 +0000

    Trades commented

--- packages/trades/position.py
+++ packages/trades/position.py
@@ -1,46 +1,85 @@
-"""
+"""Module trades.position.
+Class
+-----
+Position : Represents the position a particular client has on a particular security.
+
+
+Methods
+-------
+getSymbol() : returns symbol of the company
+getQuantity() : returns quantity of shares
+getLastModificationDate() : returns the date shares were last acquired
+setLastModificationDate() : sets the date from last acquisition
+getAcquisitionDate() : returns the date shares were first acquired
+getCurrentValue() : returns the value of the shares
+
+Functions
+----------
+__init__() : Constructor for the Position
+__str__() : Representation of the position with symbol , quantity , acquisition date and last modification date.
+
 Created on 30 Nov 2017
 
 @author: Adil Al-Yasiri
+
+Updated on 20 March 2018
+
+@author: Ibrahim Masembe
 """
 from datetime import datetime
 
+
 class Position:
     """
-    A class representing the position a particular client has on a particular security.
+    A class representing the position a particular client has on a particular security
     """
-    
+
     DATE_FORMAT = '%Y/%m/%d'
 
     def __init__(self, symbol, quantity, acquisition_date, last_modification_date=""):
-        '''
-        Constructor
-        '''
+        """
+        Constructor for the position class
+
+        Arguments
+        ---------
+        symbol: type string
+        quantity: type integer
+        acquisition_date: type date
+        lastModificationDate : type date
+
+        """
         self.symbol = symbol
         self.quantity = quantity
-        self.acqisitionDate = acquisition_date
-        self.lastModificationDate = ( acquisition_date if last_modification_date == "" else last_modification_date)
-        
+        self.acquisitionDate = acquisition_date
+        self.lastModificationDate = (acquisition_date if last_modification_date == "" else last_modification_date)
+
     def getSymbol(self):
+        """returns symbol of the company"""
         return self.symbol
-    
+
     def getQuantity(self):
+        """returns quantity of shares"""
         return self.quantity
-    
+
     def getLastModificationDate(self):
+        """returns the date shares were last acquired"""
         return self.lastModificationDate
-    
+
     def setLastModificationDate(self, value):
+        """sets the date from last acquisition"""
         self.lastModificationDate = value
-    
+
     def getAcquisitionDate(self):
-        return self.acqisitionDate
-    
+        """returns the date shares were first acquired"""
+        return self.acquisitionDate
+
     def getCurrentValue(self):
+        """returns the value of the shares"""
         pass
 
-    #where to edit print out
+    # where to edit print out
     def __str__(self):
-        return "%s|%d|%s|%s" % (self.symbol, self.quantity, 
-                                datetime.strftime(self.acqisitionDate, Position.DATE_FORMAT), 
+        # the format in which a position is printed in
+        return "%s|%d|%s|%s" % (self.symbol, self.quantity,
+                                datetime.strftime(self.acquisitionDate, Position.DATE_FORMAT),
                                 datetime.strftime(self.lastModificationDate, Position.DATE_FORMAT))
--- packages/trades/transaction.py
+++ packages/trades/transaction.py
@@ -6,6 +6,28 @@ TransactionError : Subclass of class Exception
 
 Methods
 -------
+get_quantity() : returns the quantity of shares to be acquired
+set_quantity() : sets the quantity of shares to be acquired
+get_client_id() : Returns the iD of the client.
+set_client_id() : Sets the iD of the client.
+get_symbol() : returns the symbol of the company whose shares are to be ordered
+get_trans_type() : returns the transaction type
+get_price() : returns the price of the shares
+get_date() : Gets the date when the transaction took place.
+set_symbol() : Selects the company for the transaction.
+set_trans_type() : Selects the transaction type.
+set_price() : Sets the price for the transaction
+set_date() : Sets the date when the transaction took place.
+
+
+Functions
+---------
+  __init__() : Constructor for the Transaction
+commit() : Creates or modifies existing position
+go() : Buys 20 shares with Google
+__str__() : Representation of the transaction in form of client id ,transaction date
+,transaction type, quantity,symbol and price.
+
 
 Created on 30 Nov 2017
 
@@ -30,7 +52,8 @@ class TransactionError(Exception):
 
 class Transaction:
     """
-    classdocs
+     A class representing a client order that has been executed and hence is now changed into a transaction
+
     """
     DATE_FORMAT = '%Y-%m-%d %H:%M:%S.%f'  # 2018-02-22 15:17:24.617547
 
@@ -40,7 +63,7 @@ class Transaction:
         """
         if order == "":
             return
-
+        # checks if the Order is open
         elif order.getStatus() == OrderStatus.OPEN:
             self.__clientID = int(order.getClientID())
             self.__symbol = order.getSymbol()
@@ -55,17 +78,21 @@ class Transaction:
             raise TransactionError("Order is not valid")
 
     def get_quantity(self):
+        """returns the quantity of shares to be acquired"""
         return self.__quantity
 
     def set_quantity(self, value):
+        """sets the quantity of shares to be acquired"""
         self.__quantity = value
 
     def commit(self):
         client = ClientManager.getInstance().retrieveClient(str(self.clientID))
 
+        # increases quantity of shares if buy option is selected
         if self.trans_type == TransType.BUY:
             new_position = Position(self.symbol, int(self.quantity), datetime.now())
 
+        # decreases quantity of shares if sell option is selected
         elif self.trans_type == TransType.SELL:
             new_position = Position(self.symbol, -int(self.quantity), datetime.now())
 
@@ -75,33 +102,43 @@ class Transaction:
         client.addPosition(new_position)
 
     def get_client_id(self):
+        """Returns the iD of the client."""
         return self.__clientID
 
     def set_client_id(self, value):
+        """Sets the iD of the client."""
         self.__clientID = value
 
     def get_symbol(self):
+        """returns the symbol of the company whose shares are to be ordered"""
         return self.__symbol
 
     def get_trans_type(self):
+        """returns the transaction type"""
         return self.__trans_type
 
     def get_price(self):
+        """returns the price of the shares"""
         return self.__price
 
     def get_date(self):
+        """Gets the date when the transaction took place."""
         return self.__date
 
     def set_symbol(self, value):
+        """Selects the company for the transaction."""
         self.__symbol = value
 
     def set_trans_type(self, value):
+        """Selects the transaction type."""
         self.__trans_type = value
 
     def set_price(self, value):
+        """Sets the price for the transaction."""
         self.__price = value
 
     def set_date(self, value):
+        """Sets the date when the transaction took place."""
         self.__date = value
 
     def __repr__(self):
@@ -109,8 +146,9 @@ class Transaction:
 
     # change output from here
     def __str__(self):
+        # returns a transaction as client id , transaction date,transaction type, symbol, quantity and price
         return "%s|%d|%d|%s|%.4f|%d" % (
-        self.date, self.clientID, self.trans_type, self.symbol, self.price, self.quantity)
+            self.date, self.clientID, self.trans_type, self.symbol, self.price, self.quantity)
 
         # ===========================================================================
 
